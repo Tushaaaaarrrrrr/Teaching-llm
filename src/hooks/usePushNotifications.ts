496053ed170d7902bb8eba040ff3ff5d52debf13
Laxmikant <24f3003813@ds.study.iitm.ac.in>
2026-05-09 11:26:09 +0530

    fix: TypeScript compilation errors for web-push

--- src/hooks/usePushNotifications.ts
+++ src/hooks/usePushNotifications.ts
@@ -8,7 +8,11 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64  = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData  = atob(base64)
-  return Uint8Array.from([...rawData].map(c => c.charCodeAt(0)))
+  const outputArray = new Uint8Array(rawData.length)
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
 }
 
 /**
@@ -47,7 +51,7 @@ export function usePushNotifications() {
         // Subscribe to push service
         const subscription = await reg.pushManager.subscribe({
           userVisibleOnly:      true,
-          applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+          applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY) as any,
         })
 
         const json   = subscription.toJSON()
